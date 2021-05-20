1cad9b3ec02fee1bde463c320742643192aabf2f
Max Horie <46947575+Max1259@users.noreply.github.com>
2021-05-20 16:37:57 -0700

    Update
    Error handling, remove print statements, strip strings of tabs and new lines before saving to file

--- recipe_parser.py
+++ recipe_parser.py
@@ -11,10 +11,8 @@ if len(sys.argv) != 0:
     filename = ' '.join(sys.argv[1:])
 else:
     filename = pyperclip.paste()
-print(filename)
 
 url = 'http://www.google.com/search?q=' + filename + '+recipe'
-print(url)
 
 res = requests.get(url, headers=headers).text
 soup = bs4.BeautifulSoup(res, 'lxml')
@@ -24,46 +22,51 @@ for container in soup.findAll('a', href=True):
     containers.append(container['href'])
 
 recipeURL = containers[9]
-print(recipeURL)
 
 recipeRes = requests.get(recipeURL)
 recipeSoup = bs4.BeautifulSoup(recipeRes.text, 'html.parser')
 elems = recipeSoup.select('h2, h3, h4, h5, h6')
 header = recipeSoup.select('h1')
+ingredients = []
+instructions = []
 
 for item in elems:
     if item.text == 'Ingredients':
         ingredients = item.find_next(re.compile(r'[ou]l'))
-    if item.text == 'Instructions' or item.text == 'Preparation':
+    if item.text == 'Instructions' or item.text == 'Preparation' or item.text == 'Directions':
         instructions = item.find_next(re.compile(r'[ou]l'))
-    print(item.text)
-    
-ingr = []
-for li in ingredients.find_all('li'):
-    item = li.text.encode('ascii', 'ignore')
-    item = item.decode('utf-8')
-    ingr.append(item)
 
-instr = []
-for li in instructions.find_all('li'):
-    item = li.text.encode('ascii', 'ignore')
-    item = item.decode('utf-8')
-    instr.append(item)
+if len(ingredients) > 0: 
+    ingr = []
+    for li in ingredients.find_all('li'):
+        item = li.text.encode('ascii', 'ignore')
+        item = item.decode('utf-8')
+        ingr.append(item.strip())
 
-pprint.pprint(ingr)
-pprint.pprint(instr)
+    if len(instructions) > 0:
+        instr = []
+        for li in instructions.find_all('li'):
+            item = li.text.encode('ascii', 'ignore')
+            item = item.decode('utf-8')
+            instr.append(item.strip())
+
+    pprint.pprint(ingr)
+    pprint.pprint(instr)
 
 
-file = open('C:\\Users\\maxho\\Documents\\Recipes\\' + filename + '.txt', 'w')
-file.write(header[0].text + '\n\n')
-file.write('Ingredients:\n')
+    file = open('C:\\Users\\maxho\\Documents\\Recipes\\' + filename + '.txt', 'w')
+    file.write(header[0].text + '\n')
+    file.write(recipeURL + '\n\n')
+    file.write('Ingredients:\n')
 
-for item in ingr:
-    file.write('-' + item + '\n')
+    for item in ingr:
+        file.write('-' + item + '\n')
 
-file.write('\nInstructions:\n')
+    file.write('\nInstructions:\n')
 
-for item in instr:
-    file.write('-' + item + '\n')
+    for item in instr:
+        file.write('-' + item + '\n')
 
-file.close()
+    file.close()
+else:
+    print('No ingredients found, try again')
